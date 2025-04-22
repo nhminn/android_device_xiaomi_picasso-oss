13498abda5a7f0136472cc4fbb517a38fc9ea804
hoang1007 <hoangvuhuy1007@gmail.com>
2025-04-22 22:31:31 +0700

    picasso: Welcome YAAP
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixelage_picasso.mk
+    $(LOCAL_DIR)/yaap_picasso.mk
--- yaap_picasso.mk
+++ yaap_picasso.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelAge stuff.
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Pixelage
-PIXELAGE_BUILDTYPE := UNOFFICIAL
-PIXELAGE_MAINTAINER := hoang1007
+# YAAP
+TARGET_BUILD_GAPPS := true
+TARGET_SUPPORTS_64_BIT_APPS := true
 
-PRODUCT_NAME := pixelage_picasso
+PRODUCT_NAME := yaap_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
